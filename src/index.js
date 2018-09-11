1c65f268926682fd2a886980686bb9585732cd9a
SokolR <sokolruslan92@gmail.com>
2018-09-11 12:48:02 +0300

    complete slider

--- src/index.js
+++ src/index.js
@@ -32,6 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
       callBtn.style.display = 'none';
 
       nav.style.backgroundColor = '#ffffff';
+      nav.style.boxShadow = '0 0 2px rgba(0, 0, 0, 0.2)';
 
       navBtns.style.color = '#004d80';
       navBtns.style.border = '2px solid #005173';
@@ -56,6 +57,8 @@ document.addEventListener("DOMContentLoaded", function() {
       sliderBtn.style.display = 'none';
 
       nav.style.backgroundColor = 'transparent';
+      nav.style.boxShadow = 'none';
+
 
       navBtns.style.color = '#ffffff';
       navBtns.style.border = '2px solid #ffffff';
@@ -69,5 +72,17 @@ document.addEventListener("DOMContentLoaded", function() {
         sublink.classList.remove('sublink-scrolled');
       });
     }
-  } 
+  }
+  
+  window.onload = function () {
+    let slides = document.querySelectorAll('.slider-item');
+    let currentSlide = 0;
+    var slideInterval = setInterval(nextSlide,2000);
+    
+    function nextSlide() {
+        slides[currentSlide].classList.add('hide');
+        currentSlide = (currentSlide+1)%slides.length;
+        slides[currentSlide].classList.remove('hide');
+    }
+  }
 });
