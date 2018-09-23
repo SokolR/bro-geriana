350061f1f5e22035ccad39048ced699c10dfcd46
SokolR <sokolruslan92@gmail.com>
2018-09-23 23:14:06 +0300

    change img to bg-image and refactoring js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './styles/index.css';
+// import './styles/index.css';
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -7,69 +7,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let scrolled = document.documentElement.scrollTop;
 
+    
     let pageWrapper = document.querySelector('.wrapper');
-    let nav = pageWrapper.querySelector('.global-nav');
-    let navBtns = pageWrapper.querySelector('.global-nav__btn');
-    let navLinks = pageWrapper.querySelectorAll('.global-nav__link');
-    let navSubMenu = pageWrapper.querySelector('.global-nav__submenu');
-    let navSubLink = pageWrapper.querySelectorAll('.global-nav__sublink');
-    let staticLogo = pageWrapper.querySelector('#staticLogo');
-    let scrollLogo = pageWrapper.querySelector('#scrollLogo');
-    let staticMenuIcon = pageWrapper.querySelector('#staticMenuIcon');
-    let scrollMenuIcon = pageWrapper.querySelector('#scrollMenuIcon');
-    let callBtn = pageWrapper.querySelector('#callBtn');
-    let sliderBtn = pageWrapper.querySelector('#sliderBtn');
+
+    let pageElements = pageWrapper.querySelectorAll('.page-header, .global-nav__item, .global-nav__btn, .global-nav__link, .global-nav__submenu, .global-nav__sublink, .logo, .header-btn, .slider-btn__call');
 
     if (scrolled > 100 ){
-      staticLogo.style.display = 'none';
-      scrollLogo.style.display = 'block';
-
-      staticMenuIcon.style.display = 'none';
-      scrollMenuIcon.style.display = 'inline-block';
-
-      sliderBtn.style.display = 'block';
-
-      callBtn.style.display = 'none';
-
-      nav.style.backgroundColor = '#ffffff';
-      nav.style.boxShadow = '0 0 2px rgba(0, 0, 0, 0.2)';
-
-      navBtns.style.color = '#004d80';
-      navBtns.style.border = '2px solid #005173';
-
-      navSubMenu.classList.add('submenu-scrolled');
-      
-      navLinks.forEach(link => {
-        link.style.color = '#0b2a37';
-      });
-      navSubLink.forEach(sublink => {
-        sublink.classList.add('sublink-scrolled');
+      pageElements.forEach(items => {
+        items.classList.add('scrolled');
       });
     } else {
-      staticLogo.style.display = 'block';
-      scrollLogo.style.display = 'none';
-
-      staticMenuIcon.style.display = 'inline-block';
-      scrollMenuIcon.style.display = 'none';
-
-      callBtn.style.display = 'block';
-
-      sliderBtn.style.display = 'none';
-
-      nav.style.backgroundColor = 'transparent';
-      nav.style.boxShadow = 'none';
-
-
-      navBtns.style.color = '#ffffff';
-      navBtns.style.border = '2px solid #ffffff';
-
-      navSubMenu.classList.remove('submenu-scrolled');
-
-      navLinks.forEach(link => {
-        link.style.color = '#ffffff';
-      });
-      navSubLink.forEach(sublink => {
-        sublink.classList.remove('sublink-scrolled');
+      pageElements.forEach(items => {
+        items.classList.remove('scrolled');
       });
     }
   }
@@ -84,6 +33,6 @@ document.addEventListener("DOMContentLoaded", function() {
         slides[currentSlide].classList.remove('hide');
     }
 
-    setInterval(nextSlide,7000);
+    setInterval(nextSlide, 7000);
   }
 });
